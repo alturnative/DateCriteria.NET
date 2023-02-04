b3f66e19c7c523cd9d4d53e9106aa253dbc9c275
Alex Turner <leopard.rules@gmail.com>
2023-02-04 20:11:06 +0000

    Add Utils.cs with EnumerableHashCode<T>() extension method

---
+++ src/DateCriteria.NET/Utils.cs
@@ -0,0 +1,22 @@
+﻿namespace DateCriteria.NET;
+
+public static class Utils
+{
+	/// <summary>
+	/// Produces a hash code based on the hash codes of the elements of an <see cref="IEnumerable{T}"/>. Two separate objects
+	/// containing elements producing the same hash codes will thus be considered identical, even if their references are not.
+	/// </summary>
+	/// <param name="enumerable">The enumerable collection to retrieve the hash code for.</param>
+	/// <typeparam name="T"></typeparam>
+	/// <returns></returns>
+	public static int EnumerableHashCode<T>(this IEnumerable<T> enumerable)
+	{
+		int hashCode = 0;
+		foreach (T obj in enumerable)
+		{
+			if (obj == null) continue;
+			hashCode ^= obj.GetHashCode();
+		}
+		return hashCode;
+	}
+}
